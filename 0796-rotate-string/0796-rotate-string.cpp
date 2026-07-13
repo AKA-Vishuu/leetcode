f0790b00dce517ba48c98ea9d73bd110a0cfaa9e
AKA-Vishuu <spvj22011210@gmail.com>
2026-07-14 02:15:37 +0530

    Time: 0 ms (100%), Space: 8 MB (92.88%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length() != goal.length()){
+            return false;
+        }
+        s=s+s;
+        if(s.find(goal)<s.length()){
+            return true;
+        }
+        return false;
+    }
+};
