0c69735dc806ae89ed903c814021b641036a248e
AKA-Vishuu <spvj22011210@gmail.com>
2026-07-16 20:44:20 +0530

    Time: 0 ms (100%), Space: 13.3 MB (64.21%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -3,18 +3,16 @@ public:
     int singleNumber(vector<int>& nums) {
         int n=nums.size();
 
-        int ans=0;
-        for(int i=0; i<32; i++){
-            int count=0;
-            for(int j=0; j<n; j++){
-                if((nums[j])&(1<<i)){
-                    count++;
-                }
+        sort(nums.begin(), nums.end());
+
+        int i=1;
+
+        while(i<n){
+            if(nums[i]!=nums[i-1]){
+                return nums[i-1];
             }
-            if(count%3!=0){
-                ans=ans|(1<<i);
-            }
+            i+=3;
         }
-        return ans;
+        return nums[n-1];
     }
 };
