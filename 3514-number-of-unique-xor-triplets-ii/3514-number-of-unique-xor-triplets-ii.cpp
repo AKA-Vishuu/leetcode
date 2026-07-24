6dedbcf8e5f13b14797b91023ff0397637353c2c
AKA-Vishuu <spvj22011210@gmail.com>
2026-07-24 13:20:45 +0530

    Time: 414 ms (82.02%), Space: 48.5 MB (64.61%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -2,24 +2,42 @@ class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
         int n=nums.size();
-        unordered_set<int> s;
         
-        for(int j=0; j<n; j++){
-            for(int k=0; k<n; k++){
-                s.insert(nums[j]^nums[k]);
+        int mx=0;
+        for(int i=0; i<n; i++){
+            mx=max(mx, nums[i]);
+        }
+
+        int u=1; //Unique Element Till n
+        while(u<=mx){
+            u=u<<1;
+        }
+
+        vector<int> twoPair(u);
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                twoPair[nums[i]^nums[j]]=1;
             }
         }
 
-        unordered_set<int> s2;
-        for(int i=0; i<n; i++){
-            for(auto it: s){
-                s2.insert(nums[i]^it);
+        vector<int> ans(u);
+        for(int i=0; i<u; i++){
+            if(twoPair[i]==0){
+                continue;
+            }
+
+            for(int j=0; j<n; j++){
+                ans[nums[j]^i]=1;
             }
         }
+
         int count=0;
-        for(auto k: s2){
-            count++;
+        for(int i=0; i<ans.size(); i++){
+            if(ans[i]){
+                count++;
+            }
         }
+
         return count;
     }
 };
