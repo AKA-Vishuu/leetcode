4cabe89244edd516d4ad325c0d4c5e3d9492334e
AKA-Vishuu <spvj22011210@gmail.com>
2026-07-28 20:27:19 +0530

    Time: 0 ms (100%), Space: 9.9 MB (64.83%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -14,24 +14,16 @@ public:
         if(head == NULL){
             return NULL;
         }
-        
+
         if(head->next == NULL){
             return head;
         }
-
-        int mid=1;
-        ListNode* midel = head;
-        int length=1;
-
         ListNode* temp = head;
+        ListNode* midel = head;
 
-        while(temp){
-            if(mid != ((length/2)+1)){
-                mid = (length/2)+1;
-                midel = midel->next;
-            }
-            temp = temp->next;
-            length++;
+        while(temp && temp->next){
+            midel = midel->next;
+            temp = temp->next->next;
         }
         return midel;
     }
