84b14e6e9de6418cc3b470626638e35d8f505066
AKA-Vishuu <spvj22011210@gmail.com>
2026-07-29 01:00:16 +0530

    Time: 8 ms (7.06%), Space: 77.7 MB (13.12%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -0,0 +1,78 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+        vector<int> vec;
+
+        int carry = 0;
+
+        while(l1!=NULL && l2!=NULL){
+            if(((l1->val) + (l2->val) + carry) > 9){
+
+                vec.push_back(((l1->val) + (l2->val) + carry)%10);
+                carry = 1;
+            }
+            else{
+                vec.push_back(((l1->val) + (l2->val) + carry)%10);
+                carry=0;
+            }
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+
+        while(l1 != NULL){
+            if(((l1->val) + carry) > 9){
+
+                vec.push_back(((l1->val) + carry)%10);
+                carry = 1;
+            }
+            else{
+                vec.push_back(((l1->val) + carry)%10);
+                carry=0;
+            }
+            l1 = l1->next;
+        }
+
+        while(l2 != NULL){
+            if(((l2->val) + carry) > 9){
+
+                vec.push_back(((l2->val) + carry)%10);
+                carry = 1;
+            }
+            else{
+                vec.push_back(((l2->val) + carry)%10);
+                carry=0;
+            }
+            l2 = l2->next;
+        }
+
+        if(carry == 1){
+            vec.push_back(1);
+        }
+
+        ListNode* head = new ListNode(vec[0]);
+
+        ListNode* temp = head;
+
+        for(int i=1; i<vec.size(); i++){
+            ListNode* newNode = new ListNode(vec[i]);
+            temp->next = newNode;
+            temp = newNode;
+        }
+
+        temp->next == NULL;
+
+        return head;
+
+    }
+};
