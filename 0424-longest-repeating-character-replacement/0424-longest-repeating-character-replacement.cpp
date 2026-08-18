32d3b42682faab2ad2bec7fd623a006f4c98d330
AKA-Vishuu <301701360+AKA-Vishuu@users.noreply.github.com>
2026-08-18 14:46:16 +0530

    Time: 0 ms (100%), Space: 10.9 MB (71.49%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n = s.length();
+
+        int l = 0;
+        int r = 0;
+
+        int hash[26] = {0};
+
+        int maxFreq = 0;
+        int maxLength = 0;
+
+        while(r<n){
+            hash[s[r]-'A']++;
+            maxFreq = max(maxFreq, hash[s[r]-'A']);
+            
+            if(((r-l+1)-maxFreq) > k){
+                hash[s[l]-'A']--;
+                l++;
+            }
+            maxLength = max(maxLength, (r-l+1));
+            r++;
+        }
+        return maxLength;
+        
+    }
+};
